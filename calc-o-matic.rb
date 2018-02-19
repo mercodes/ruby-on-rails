61e558dd2bf6a32ef49a68ff33568f75468a8902
mercodes <36412718+mercodes@users.noreply.github.com>
2018-02-19 15:57:06 +0200

    Update calc-o-matic.rb

--- calc-o-matic.rb
+++ calc-o-matic.rb
@@ -1,3 +1,5 @@
+#Name Analyzer 
+
 puts "Hello! What is your first name?"
 first_name = gets.chomp
 puts "Lovely! And your last name?"
@@ -7,21 +9,22 @@ puts "According to my calculations, your first name has #{first_name.length} cha
 full_name = first_name + " " + last_name
 puts "Also, your name reversed reads #{full_name.reverse}. Pretty cool, huh?"
 
+#Calculator
 
 def multiply(first_number,second_number)
-   first_number.to_f * second_number.to_f
+  first_number.to_f * second_number.to_f
 end
 
 def divide(first_number,second_number)
-   first_number.to_f / second_number.to_f
+  first_number.to_f / second_number.to_f
 end
 
 def subtract(first_number,second_number)
-   second_number.to_f - first_number.to_f
+  second_number.to_f - first_number.to_f
 end
 
 def mod(first_number,second_number)
-   first_number.to_f % second_number.to_f
+  first_number.to_f % second_number.to_f
 end
 
 puts "Let's get to the math part now. Would you like to:"
@@ -37,21 +40,22 @@ puts "Now please input a second number"
 second_number = gets.chomp 
 
 if prompt == '1'
-	puts "You have chosen to multiply"
-	result = multiply(first_number,second_number)
-	puts "The result is #{result}. Have a nice day!"
+  puts "You have chosen to multiply"
+  result = multiply(first_number,second_number)
+  puts "The result is #{result}. Have a nice day!"
 elsif prompt == '2'
-	puts "You have chosen to divide"
-	result = divide(first_number,second_number)
-	puts "The result is #{result}. Have a nice day!"
+  puts "You have chosen to divide"
+  result = divide(first_number,second_number)
+  puts "The result is #{result}. Have a nice day!"
 elsif prompt == '3'
-	puts "You have chosen to subtract"
-	result = subtract(first_number,second_number)
-	puts "The result is #{result}. Have a nice day!"
+  puts "You have chosen to subtract"
+  result = subtract(first_number,second_number)
+  puts "The result is #{result}. Have a nice day!"
 elsif prompt == '4'
-	puts "You have chosen to find the remainder"
-	result = mod(first_number,second_number)
-	puts "The result is #{result}. Have a nice day!"
+  puts "You have chosen to find the remainder"
+  result = mod(first_number,second_number)
+  puts "The result is #{result}. Have a nice day!"
 else 
-	puts "You have made an invalid choice"
+  puts "You have made an invalid choice"
 end
+
